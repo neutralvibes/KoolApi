1fafc1856a94d814472b2f6e6d8c58bd577078f7
neutralvibes <neutralvibes@gmail.com>
2023-03-13 21:57:23 +0000

    fix handler matching

--- src/KoolApi.h
+++ src/KoolApi.h
@@ -1,8 +1,6 @@
 #ifndef __KOOLAPI_H__
 #define __KOOLAPI_H__
 
-
-
 /*
 
 https://restfulapi.net/resource-naming/
@@ -42,14 +40,14 @@ https://restfulapi.net/resource-naming/
 
 /**
  * @brief Handles processing of requests
- * 
+ *
  */
 class KoolApi
 {
 public:
   /**
    * @brief Construct a KoolApi object
-   * 
+   *
    * @param urlBase uri to mount api on. Eg "/_api"
    */
   KoolApi(const char *urlBase);
@@ -58,24 +56,24 @@ public:
 
   /**
    * @brief Returns number of uri endpoints registered
-   * 
-   * @return const uint8_t 
+   *
+   * @return const uint8_t
    */
   const size_t uriCount() const;
 
   /**
    * @brief Get the Url Base set
-   * 
-   * @return const char* 
+   *
+   * @return const char*
    */
   const char *const getUrlBase() const;
 
   /**
    * @brief Set the key to determine the uri path.
    *  For when the source of the request may be other than webserver, such as Mqtt, Serial, Websockets etc...
-   * 
+   *
    * @param key The json key to examine. Default: "$_uri"
-   * @return ApiProcessor& 
+   * @return ApiProcessor&
    */
 
   KoolApi &setRequestKey(const char *key);
@@ -89,39 +87,39 @@ public:
 
   /**
    * @brief Set the Uri return json key.
-   * 
+   *
    * If used will add the uri to the returned object automatically for you.
-   * 
+   *
    * @param key Name of json key to hold the uri path.
-   * @return ApiProcessor& 
+   * @return ApiProcessor&
    */
   KoolApi &setUriKey(const char *key);
 
   /**
    * @brief Get the uri Key to be added to returned results.
-   * 
-   * @return const char* 
+   *
+   * @return const char*
    */
   const char *const getUriKey() const;
 
   /**
    * @brief If set will enable the describer on the uri specified for GET requests.
-   * 
+   *
    * @param uri eg `__describer__`. Default: nullptr
-   * @return KoolApi& 
+   * @return KoolApi&
    */
   KoolApi &setDesriberUri(const char *uri);
 
   /**
-   * @brief Return the uri for the Desriber. 
-   * 
+   * @brief Return the uri for the Desriber.
+   *
    * @return const char* The uri. Returns nullptr if not set.
    */
   const char *const getDesriberUri() const;
 
   /**
    * @brief Add a uri handler
-   * 
+   *
    * @param uri uri path. Eg "/puppet"
    * @param handler uri handling class
    */
@@ -129,9 +127,9 @@ public:
 
   /**
    * @brief Process the request.
-   * 
+   *
    * If the methods `methodsAccepted` is set, anything other than those methods will respond 405 Not Allowed.
-   * 
+   *
    * @param request The request object
    * @param methodsAccepted bitwise accepted methods. eg  (API_METHOD_GET | API_METHOD_PUT)
    */
@@ -139,10 +137,10 @@ public:
 
   /**
    * @brief Checks if the url supplied starts with the base url
-   * 
-   * @param url 
-   * @return true 
-   * @return false 
+   *
+   * @param url
+   * @return true
+   * @return false
    */
   inline bool startsWithUriKey(const char *url) const;
 
@@ -150,18 +148,18 @@ public:
 
   /**
    * @brief Api filter for Async Webserver
-   * 
+   *
    * @param r request
-   * @return true 
-   * @return false 
+   * @return true
+   * @return false
    */
   bool apiFilter(const AsyncWebServerRequest *r);
 
   /**
    * @brief Register api with AsyncWebserver instance
-   * 
-   * @param server 
-   * @param logfunc 
+   *
+   * @param server
+   * @param logfunc
    */
   void registerWith(AsyncWebServer &server, void (*logfunc)(AsyncWebServerRequest *request, const char *msg) = nullptr);
 
@@ -170,37 +168,37 @@ public:
 protected:
   /**
    * @brief request json key name to map to api path
-   * 
+   *
    */
   const char *_requestKey = "$_uri";
 
-  /** 
+  /**
    * Url base for webserver requests
    */
   const char *_urlBase = nullptr;
 
   /**
    * @brief Key name to populate with api path that responded to request.
-   * 
+   *
    * If not specified will not be created.
-   * 
+   *
    */
   const char *_uriKey = nullptr;
 
   /**
    * Handler class list
    */
-  koolutils::LinkedList<KoolApiPath*> _handlerList;
+  koolutils::LinkedList<KoolApiPath *> _handlerList;
 
   /**
    * @brief If set will enable the describer on the uri specified
-   * 
+   *
    */
   const char *_describerUri = nullptr;
 
   /**
    * @brief Returns the handler for the path specified
-   * 
+   *
    * @param path Path to search for
    * @return ApiPath* || nullptr if not found
    */
@@ -208,21 +206,20 @@ protected:
 
   /**
    * @brief Adds options for describing the api
-   * 
-   * @param out 
+   *
+   * @param out
    */
   void _describeApi(JsonObject &out);
 
 private:
 };
 
-
-KoolApi::KoolApi(const char *urlBase) : _urlBase(urlBase), _handlerList(koolutils::LinkedList<KoolApiPath*>())
+KoolApi::KoolApi(const char *urlBase) : _urlBase(urlBase), _handlerList(koolutils::LinkedList<KoolApiPath *>())
 {
 }
 
-KoolApi::~KoolApi() 
-{ 
+KoolApi::~KoolApi()
+{
 }
 
 const size_t KoolApi::uriCount() const
@@ -327,9 +324,10 @@ KoolApiPath *KoolApi::_findHandler(const char *path)
   if (path)
   {
 
-    for (uint8_t i = 0; i <_handlerList.size(); ++i)
+    for (uint8_t i = 0; i < _handlerList.size(); ++i)
     {
-      if (strncmp(_handlerList[i]->_path, path, strlen(path)) == 0)
+
+      if (strcmp(_handlerList[i]->_path, path) == 0)
       {
         return _handlerList[i];
       }
@@ -343,8 +341,8 @@ void KoolApi::_describeApi(JsonObject &out)
 {
   auto h = out.createNestedArray("handlers");
 
-    for (uint8_t i = 0; i <_handlerList.size(); ++i)
-    {
+  for (uint8_t i = 0; i < _handlerList.size(); ++i)
+  {
 
     JsonObject p = h.createNestedObject();
     p["path"] = _handlerList[i]->_path;
@@ -357,7 +355,6 @@ KoolApi &KoolApi::setUriKey(const char *key)
   _uriKey = key;
   return *this;
 }
-
 
 #ifdef _ESPAsyncWebServer_H_
 
@@ -376,8 +373,7 @@ void KoolApi::registerWith(AsyncWebServer &server, void (*logfunc)(AsyncWebServe
                 logfunc(request, "");
 
               ApiAsyncWebRequest apiRequest(request);
-              process(apiRequest);
-            })
+              process(apiRequest); })
       .setFilter(std::bind(&KoolApi::apiFilter, this, std::placeholders::_1));
 
   server.on(
@@ -389,16 +385,14 @@ void KoolApi::registerWith(AsyncWebServer &server, void (*logfunc)(AsyncWebServe
                 auto resp = request->beginResponse(200);
                 resp->addHeader("Access-Control-Allow-Methods", "*");
                 request->send(resp);
-              }
-            },
+              } },
             NULL, [this, logfunc](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
             {
               if (logfunc)
                 logfunc(request, "");
 
               ApiAsyncWebRequest apiRequest(request, data, len);
-              process(apiRequest);
-            })
+              process(apiRequest); })
       .setFilter(std::bind(&KoolApi::apiFilter, this, std::placeholders::_1));
 }
 
